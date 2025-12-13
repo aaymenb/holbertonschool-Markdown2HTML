9516f729fb57263c8bb436ebf0d21c96730c46cb
AymenB <aymen1996ben@gmail.com>
2025-12-13 15:04:05 +0100

    project1week2

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,7 @@ from os import path
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
 
@@ -12,17 +12,4 @@ if __name__ == "__main__":
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
         sys.exit(1)
 
-    with open(sys.argv[1], "r") as md, open(sys.argv[2], "w") as html:
-        for line in md:
-            line = line.rstrip("\n")
-
-            count = 0
-            for c in line:
-                if c == "#":
-                    count += 1
-                else:
-                    break
-
-            if count > 0 and count <= 6 and line[count] == " ":
-                content = line[count+1:]
-                html.write(f"<h{count}>{content}</h{count}>\n")
+    sys.exit(0)
