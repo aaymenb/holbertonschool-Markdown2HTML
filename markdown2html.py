b44fe52778b128484a3100d7b89c602cdb37c6f3
AymenB <aymen1996ben@gmail.com>
2025-12-13 15:46:50 +0100

    project1week2

--- markdown2html.py
+++ markdown2html.py
@@ -2,18 +2,18 @@
 import sys
 import os
 
-# Vérifie qu'on a bien 2 arguments (nom du fichier + fichier de sortie)
-if len(sys.argv) != 3:
-    print(f"Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+# Vérifie le nombre d'arguments
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# Vérifie que le fichier Markdown existe
-if not os.path.isfile(input_file):
+# Vérifie si le fichier Markdown existe
+if not os.path.exists(input_file):
     print(f"Missing {input_file}", file=sys.stderr)
     sys.exit(1)
 
-# Si tout va bien, on ne fait rien et on sort avec code 0
-sys.exit(0)
+# Si tout va bien
+sys.exit(0) 
